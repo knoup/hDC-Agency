f17a2ab9e61259bc0b2089e3224b82e9c02ac94b
knoup <64130006+knoup@users.noreply.github.com>
2020-11-05 21:18:31 -0300

    Finalização do projeto

--- js/scripts.js
+++ js/scripts.js
@@ -149,4 +149,44 @@ $(document).ready(function() {
             });
         }
     }
+
+    //Scroll para seções
+    let navBtn = $('.nav-item');
+
+    let bannerSection = $('home-menu');
+    let aboutSection = $('#about-area');
+    let servicesSection = $('#services-area');
+    let teamSection = $('#team-area');
+    let portfolioSection = $('#portfolio-area');
+    let contactSection = $('#contact-area');
+
+    let scrollTo = '';
+
+    $(navBtn).click(function(){
+
+        let btnId = $(this).attr('id');
+
+        console.log(btnId);
+        if(btnId=='about-menu') {
+            scrollTo = aboutSection;
+        } else if(btnId = 'services-menu') {
+            scrollTo = servicesSection;
+        } else if(btnId = 'team-menu') {
+            scrollTo = teamSection;
+        } else if(btnId = 'portfolio-menu') {
+            scrollTo = portfolioSection;
+        } else if(btnId = 'contact-menu') {
+            scrollTo = contactSection;
+        } else {
+            scrollTo = bannerSection;
+        }
+
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(scrollTo).offset().top - 70
+    
+        }, 1500);
+    
+    });
+
+    
 });

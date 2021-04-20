705de57553353b1f71e26071c938c900fe60c57e
Pedro Manoel <dev.pedromanoel@pm.me>
2021-04-19 21:00:24 -0300

    🐛fix(scroll): Scroll to page's sections

--- js/scripts.js
+++ js/scripts.js
@@ -155,7 +155,7 @@ $(document).ready(function() {
 
     let bannerSection = $('home-menu');
     let aboutSection = $('#about-area');
-    let servicesSection = $('#services-area');
+    let servicesSection = $('#service-area');
     let teamSection = $('#team-area');
     let portfolioSection = $('#portfolio-area');
     let contactSection = $('#contact-area');
@@ -169,13 +169,13 @@ $(document).ready(function() {
         console.log(btnId);
         if(btnId=='about-menu') {
             scrollTo = aboutSection;
-        } else if(btnId = 'services-menu') {
+        } else if(btnId == 'services-menu') {
             scrollTo = servicesSection;
-        } else if(btnId = 'team-menu') {
+        } else if(btnId == 'team-menu') {
             scrollTo = teamSection;
-        } else if(btnId = 'portfolio-menu') {
+        } else if(btnId == 'portfolio-menu') {
             scrollTo = portfolioSection;
-        } else if(btnId = 'contact-menu') {
+        } else if(btnId == 'contact-menu') {
             scrollTo = contactSection;
         } else {
             scrollTo = bannerSection;
